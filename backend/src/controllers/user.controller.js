95c1e10a95dcb48e6970aac87922ec318806b263
deepak <deepanbind@cyberwala.in>
2025-08-31 01:50:07 +0530

    bcryct js change

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,13 @@
 import httpStatus from "http-status";
 import { User } from "../models/user.model.js";
-import bcrypt, { hash } from "bcrypt"
+// import bcrypt, { hash } from "bcrypt"
+const bcrypt = require("bcryptjs");
+
 
 import crypto from "crypto"
 import { Meeting } from "../models/meeting.model.js";
+
+
 const login = async (req, res) => {
 
     const { username, password } = req.body;
